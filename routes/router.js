b2c414d00a8dff3f0c60d7b1f46dd23868ff4304
CptPow <capitanpow@gmail.com>
2023-12-01 19:28:06 +0100

    update

--- routes/router.js
+++ routes/router.js
@@ -2,41 +2,49 @@ const express = require("express");
 const router = express.Router();
 const posts = require("./posts.js");
 
-
+function paramsController(req, res) {
+  const user = req.query.user;
+  const authToken = req.query["auth-token"];
+  if (user === "admin" && authToken === "admin123") {
+    res.send("Richiesta confermata");
+  } else {
+    res.send("Errore nella richiesta");
+  }
+}
 
 module.exports = router
-.get("/", (req, res) => {
-    res.status(200).json(posts)
-})
+  .get("/", (req, res) => {
+    res.status(200).json(posts);
+  })
 
-
-.get("/:id", (req, res) => {
+  .get("/:id", (req, res) => {
     const post = posts.find((p) => p.id === parseInt(req.params.id));
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      return res.json({ status: 404, message: "id non trovato" });
+    }
+  })
+
+  .get("/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find(post => post.slug === slug);
+
     if (post) {
         res.status(200).json(post)
     } else {
-        return res.json({status: 404, message: "id non trovato"})
+        res.status(404).send("Post non trovato")
     }
-})
+  })
 
+  .post("/", (req, res) => {
+    paramsController(req, res);
+  })
 
-.post("/", (req, res) => {
-    const params = req.url;
-    if (params === "/?user=admin&auth-token=admin123"){
-        res.send("Sei loggato con metodo POST")
-    } else {
-        res.send("Log in non riuscito con metodo POST")
-    }
-}) 
-.patch("/", (req, res) => {
-    const params = req.url;
-    if (params === "/?user=admin&auth-token=admin123"){
-        res.send("Sei loggato con metodo PATCH")
-    } else {
-        res.send("Log in non riuscito con metodo PATCH")
-    }
-}) 
+  .patch("/", (req, res) => {
+    paramsController(req, res);
+  })
 
-
-
-
+  .delete("/", (req, res) => {
+    paramsController(req, res);
+  });
